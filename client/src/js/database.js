af7c9c301f3393d9fc5cbac00c37d0cfae296f06
jregues <jregues@me.com>
2024-05-26 11:59:14 -0400

    made changes to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,25 +1,3 @@
-// import { openDB } from 'idb';
-
-// const initdb = async () =>
-//   openDB('jate', 1, {
-//     upgrade(db) {
-//       if (db.objectStoreNames.contains('jate')) {
-//         console.log('jate database already exists');
-//         return;
-//       }
-//       db.createObjectStore('jate', { keyPath: 'id', autoIncrement: true });
-//       console.log('jate database created');
-//     },
-//   });
-
-// // TODO: Add logic to a method that accepts some content and adds it to the database
-// export const putDb = async (content) => console.error('putDb not implemented');
-
-// // TODO: Add logic for a method that gets all the content from the database
-// export const getDb = async () => console.error('getDb not implemented');
-
-// initdb();
-
 import { openDB } from 'idb';
 
 const initDb = async () => 
@@ -68,5 +46,5 @@ export const getDb = async () => {
 };
 
 // Call the method to get all content from the database
-getDb();
 initDb()
+getDb();
